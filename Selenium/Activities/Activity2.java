f03c125ed3913c8978ed84c81dfcda6ed9376ce7
qaarnab <106547647+qaarnab@users.noreply.github.com>
2022-05-31 00:12:19 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity2.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity2 {
+    public static void main(String args[]){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.training-support.net");
+        System.out.println(driver.getTitle());
+
+        WebElement idLocator = driver.findElement(By.id("about-link"));
+        System.out.println("Text in element: " + idLocator.getText());
+
+        WebElement classNameLocator = driver.findElement(By.className("green"));
+        System.out.println("Text in element: " + classNameLocator.getText());
+
+        WebElement cssLocator = driver.findElement(By.cssSelector(".green"));
+        System.out.println("Text in element: " + cssLocator.getText());
+
+        WebElement linkTextLocator = driver.findElement(By.linkText("About Us"));
+        System.out.println("Text in element: " + linkTextLocator.getText());
+        driver.quit();
+    }
+}
